d697f7756a6b1ebbd61e0db7d1e347ecbe76d6f4
苏瑞梦 <934274351@qq.com>
2020-07-29 14:58:27 +0800

    111

--- glsl1/screen.fs
+++ glsl1/screen.fs
@@ -5,7 +5,7 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 const float offset = 1.0/1024;  
-
+uniform int uv;
 //void main()
 //{
 //	FragColor = texture(screenTexture, TexCoords);
@@ -19,11 +19,16 @@ const float offset = 1.0/1024;
 
 void main()
 {
-	vec3 col = vec3(texture(screenTexture, TexCoords));
-	if(col.x!=1.0f&&col.y!=1.0f&&col.z!=1.0f){
-		FragColor = vec4(vec3(1.0 - col), 1.0);
+	if(uv==0){
+		FragColor = vec4(vec3(0.5), 1.0);
 	}else{
-		FragColor = texture(screenTexture, TexCoords);
+		FragColor = vec4(vec3(0.0), 1.0);
 	}
+	//vec3 col = vec3(texture(screenTexture, TexCoords));
+	//if(col.x!=1.0f&&col.y!=1.0f&&col.z!=1.0f){
+	//	FragColor = vec4(vec3(1.0 - col), 1.0);
+	//}else{
+	//	FragColor = texture(screenTexture, TexCoords);
+	//}
 	
 }
