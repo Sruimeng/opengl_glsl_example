72af2c3456c8f630d44984b53dead5ab79b74175
苏瑞梦 <934274351@qq.com>
2020-08-12 17:57:37 +0800

    Update track_ball.h

--- toy/track_ball.h
+++ toy/track_ball.h
@@ -1,6 +1,7 @@
 //轨迹球控制代码
 
 #pragma once
+#include "camera.h"
 namespace toy {
 	
 	class trackBall
@@ -10,17 +11,16 @@ namespace toy {
 		~trackBall();
 
 	private:
-		bool m_pan = false;
+		bool m_panLocked = false;
 		bool m_zoomLocked = false;
+		bool m_rotLocked = false;
+		float        m_moveSpeed = 1.0f;
+		float        m_rollSpeed = 0.5f;
+		Camera* m_camera; //相机
+
 		//鼠标相关参数
 		int m_prevPosX;
+		int m_prevPosY;
+
 	};
-
-	trackBall::trackBall()
-	{
-	}
-
-	trackBall::~trackBall()
-	{
-	}
 }
